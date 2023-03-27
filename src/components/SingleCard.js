cd30cfff5311c32d399d915e33a5ec1e482a7c3b
FreddieCodes <FrederickCodes@gmail.com>
2023-03-27 21:22:08 +0100

    moved card template to own component

---
+++ src/components/SingleCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function SingleCard({ id, src }) {
+  return (
+    <div className="card" key={id}>
+      <div>
+        <img className="front" src={src} alt="card front" />
+        <img className="back" src="/img/cover.png" alt="card back" />
+      </div>      
+    </div>
+  )
+}
